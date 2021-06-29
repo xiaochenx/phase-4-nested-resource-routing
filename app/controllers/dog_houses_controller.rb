976778f87ea017d8952fd3c25ee9c0bcb505460b
Xiaochen Xu <xiaochex@gmail.com>
2021-06-29 18:53:43 -0400

    commit tree 4aca2ae8728cec65e792e258445e491c85395ddf

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,10 +1,26 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    dog_houses = DogHouse.all
+    render json: dog_houses
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
   end
+
+  def reviews_index
+    dog_house = DogHouse.find(params[:dog_house_id])
+    reviews = dog_house.reviews
+    render json: reviews, include: :dog_house
+  end
+
+  def review
+    review = Review.find(params[:id])
+    render json: review, include: :dog_house
+  end 
 
   private
 
